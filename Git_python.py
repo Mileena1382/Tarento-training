07926ac0a3a78aca50de89face6bb9f016c32039
Mileena1382 <mileenamartin13@gmail.com>
2023-05-22 11:01:16 +0530

    First Commit

---
+++ Git_python.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+num=int(input("Enter the number :"))
+if(num%2==0):
+    print(num," is an Even number")
+else:
+    print(num," is an Odd number")
+
+
+# In[ ]:
+
+
+
+
