1dc9616cf36a4865364602d5745fa552ba90c6f5
Mileena1382 <75910416+Mileena1382@users.noreply.github.com>
2023-07-23 11:45:08 +0530

    Restored deleted files

---
+++ Git_python.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+num=int(input("Enter the number :"))
+if(num%2==0):
+    print(num," is an even number")
+else:
+    print(num," is an  Odd number")
+
+
+
+
+# In[ ]:
+
+
+
+
